85bffea3c18cc4187994ee1e59f75f655f64b3fe
Abdul Rehman <160416267+iamabd55@users.noreply.github.com>
2025-08-28 21:55:12 +0500

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -50,7 +50,7 @@ const Dashboard = () => {
     setLoading(true);
     try {
       const API = import.meta.env.VITE_API_URL;
-      const res = await axios.get("{API}/api/users/dashboard", {
+      const res = await axios.get(`{API}/api/users/dashboard`, {
         headers: { Authorization: `Bearer ${token}` },
       });
 
@@ -394,4 +394,5 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
 
